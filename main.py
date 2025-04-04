eb5bbc2a07b25ef662905174f8d2d5cd23fd9971
IceFairy_Cirno <156689366+IceFairyCirno@users.noreply.github.com>
2025-04-05 00:36:04 +0800

    Restructured python and changed dataset

--- main.py
+++ main.py
@@ -5,41 +5,49 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-data = pd.read_csv("archive\Heart_disease_statlog.csv")
+def train(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=15)
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+    model = LogisticRegression()
+    model.fit(x_train_scaled, y_train)
+    y_pred = model.predict(x_test_scaled)
+    accuracy = round(accuracy_score(y_test, y_pred)*100, 2)
+    return model, scaler, accuracy
 
-x = data.drop("target", axis=1)
-y =  data["target"]
+def main():
+    data = pd.read_csv("archive\Heart_disease_cleveland_new.csv")
+    x = data.drop("target", axis=1)
+    y =  data["target"]
+    model, scaler, accuracy =train(x, y)
+    print(f"This model's accuracy: {accuracy}%")
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=15)
+    user_info = input("Enter your age and sex (e.g. 60 M): ").split(" ")
+    age, sex = int(user_info[0]), (1 if user_info[1]=="M" or "m" else 0)
 
-scaler = StandardScaler()
+    df = pd.DataFrame(x)
+    df = df[df['age'] == age]
+    df = df[df['sex'] == sex]
+    defaults = df.median()
+    defaults = pd.DataFrame(defaults).T
 
-x_train_scaled = scaler.fit_transform(x_train)
-x_test_scaled = scaler.transform(x_test)
+    if defaults.isna().all().all() == True:
+        print("No info related")
+    else:
+        x_client = scaler.transform(defaults)
+        prediction = model.predict(x_client)
+        chance = model.predict_proba(x_client)
+        print(f"There is a {(chance[:, (1 if prediction else 0)]*100)[0]:.1f}% chance that you are {"having" if prediction else "not having"} a heart disease")
 
-model = LogisticRegression()
-model.fit(x_train_scaled, y_train)
+main()
 
-y_pred = model.predict(x_test_scaled)
-print(f"This model's accuracy: {(accuracy_score(y_test, y_pred)*100):.2f}%")
 
-info = input("Enter your age and sex (e.g. 60 M): ").split(" ")
-age, sex = int(info[0]), (1 if info[1]==("M" or "m") else 0)
 
-df=pd.DataFrame(x_train)
-df = df[df['age'] == age]
-df = df[df['sex'] == sex]
 
-defaults = df.median()
-defaults = pd.DataFrame(defaults).T
 
-if defaults.isna().all().all() == True:
-    print("No info related")
-else:
-    x_client = scaler.transform(defaults)
-    prediction = model.predict(x_client)
-    chance = model.predict_proba(x_client)
-    print(f"Heart Disease: {"Yes" if prediction else "No"} with a chance of {(chance[:, (1 if prediction else 0)]*100)[0]:.1f}%")
+
+
 
 
 
